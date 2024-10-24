4679034518eba305f8c1de4644886e0bbfb19023
Kelby Rogers <kelby_rogers@trimble.com>
2024-10-25 08:36:48 +1300

    Use a custom shader to avoid crash

--- app/src/main/assets/main.js
+++ app/src/main/assets/main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { GLTFLoader } from "./GLTFLoader.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -19,36 +20,65 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-let cube;
-let geo = new THREE.BoxGeometry();
-let material = new THREE.MeshPhongMaterial({ color: "green" });
-cube = new THREE.Mesh(geo, material);
+const gLTFLoader = new GLTFLoader();
 
-scene.add(cube);
+const material = new THREE.ShaderMaterial({
+  uniforms: {
+    lightPosition: { type: "v3", value: new THREE.Vector3(5, 5, 5) },
+  },
+  vertexShader: `
+  varying vec3 vNormal;
+  varying vec3 vWorldPosition;
+  void main() {
+    vNormal = normalize(normalMatrix * normal);
+    vWorldPosition = (modelMatrix * vec4(position, 1.0)).xyz;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  }
+`,
+  fragmentShader: `
+  uniform vec3 lightPosition;
+  varying vec3 vNormal;
+  varying vec3 vWorldPosition;
+  void main() {
+    vec3 lightDir = normalize(lightPosition - vWorldPosition);
+    float brightness = max(dot(vNormal, lightDir), 0.0);
+    vec3 reflectedColor = vec3(0.5, 0.5, 0.5);
+    gl_FragColor = vec4(reflectedColor * brightness, 1.0);
+  }
+`,
+});
 
-const dialog = document.querySelector("#dialog");
-dialog.showModal();
+gLTFLoader.loadAsync("./hex.glb").then((gltfScene) => {
+  gltfScene.scene.traverse((child) => {
+    if (child instanceof THREE.Mesh) {
+      child.material = material;
+    }
+  });
+  let hex = gltfScene.scene;
+  scene.add(hex);
 
-window.addEventListener("touchstart", closeDialogAndAnimate);
-window.addEventListener("click", closeDialogAndAnimate);
+  const dialog = document.querySelector("#dialog");
+  dialog.showModal();
 
-function closeDialogAndAnimate() {
-  dialog.close();
-  animate();
-}
+  window.addEventListener("touchstart", closeDialogAndAnimate);
+  window.addEventListener("click", closeDialogAndAnimate);
 
-// Animation loop
-function animate() {
-  requestAnimationFrame(animate);
-
-  // Rotate the cube
-  if (cube) {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+  function closeDialogAndAnimate() {
+    dialog.close();
+    animate();
   }
 
-  renderer.render(scene, camera);
-}
+  // Animation loop
+  function animate() {
+    requestAnimationFrame(animate);
+
+    if (hex) {
+      hex.rotation.y += 0.01;
+    }
+
+    renderer.render(scene, camera);
+  }
+});
 
 // Handle window resizing
 window.addEventListener("resize", () => {
